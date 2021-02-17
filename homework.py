a83e0f149767191818939aeac558a3fab8cba9d4
nicksas <sidorenko.nikita.1999@gmail.com>
2021-02-17 23:33:13 +0300

    first commit

--- homework.py
+++ homework.py
@@ -1,9 +1,18 @@
 import datetime as dt
 
-
 class Calculator:
-    pass
+    def __init__(self, limit, records):
+        self.limit = limit
+        self.records = []
+
 
 
 class Record:
-    pass
+    def __init__(self, amount, comment, date):
+        self.amount = amount
+        self.comment = comment
+        self.date = date
+
+
+class CashCalculator:
+    def __init__(self):
