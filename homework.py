bd9751edb664bc769fe90ad447ecc35cdb782255
nicksas <sidorenko.nikita.1999@gmail.com>
2021-03-03 01:04:05 +0300

    first commit

--- homework.py
+++ homework.py
@@ -1,5 +1,8 @@
 import datetime as dt
 
+
+# date_format = '%d.%m.%Y'
+
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
@@ -12,9 +15,9 @@ class Calculator:
         count_today = 0
         today = dt.datetime.now().date()
         for rec in self.records:
-            if rec.date == today :
+            if rec.date == today:
                 count_today += rec.amount
-        return count_today                    
+        return count_today
 
     def get_week_stats(self):
         count_week = 0
@@ -23,20 +26,21 @@ class Calculator:
         for rec in self.records:
             if week_day >= rec.date <= today:
                 count_week += rec.amount
-        return count_week        
+        return count_week
+
+    def showStat(self):
+        return self.records
 
 
-
-
-
-class Record(Calculator):
-    def __init__(self, amount, comment, date=''):
+class Record:
+    def __init__(self, amount, comment, date=""):
         self.amount = amount
         self.comment = comment
-        if date == '':
-                self.date = dt.datetime.now().date()
+        if date == "":
+            self.date = dt.datetime.now().date()
         else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y')       
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y')
+
 
 class CashCalculator(Calculator):
     USD_RATE = 75
@@ -44,9 +48,9 @@ class CashCalculator(Calculator):
     EURO_RATE = 90
 
     currencies = {
-            'usd': (USD_RATE, 'USD'),
-            'euro': (EURO_RATE, 'EURO'),
-            'rub': (RUB_RATE, 'руб'),
+        'usd': (USD_RATE, 'USD'),
+        'euro': (EURO_RATE, 'EURO'),
+        'rub': (RUB_RATE, 'руб'),
     }
 
     def get_today_cash_remained(self, currency):
@@ -55,19 +59,22 @@ class CashCalculator(Calculator):
         dict = CashCalculator.currencies
         for cur in dict:
             if cur == currency:
-                for i in dict[cur].split():
-                    count_cur = today_stats/dict[cur].split[1]
+                limit_rate = limit / dict[cur][0]
+                today_stats_rate = today_stats / dict[cur][0]
+                current_cur = dict[cur][1]
+                if today_stats_rate > limit_rate:
+                    count_loan = today_stats_rate - limit_rate
+                    print(f'Денег нет, держись: твой долг {count_loan} {current_cur}')
 
-                    if count_cur < (limit/dict[cur].split[1]):
-                        print(f'На сегодня осталось {count_cur} {dict[cur].split[2]}')
+                if today_stats_rate == limit_rate:
+                    print('Денег нет, держись')
 
-                    elif count < (limi)
-        
-#ыплит в словаре не работает, обдумать!!!!
-        
-    
+                if today_stats_rate < limit_rate:
+                    count = limit_rate - today_stats_rate
+                    print(f'На сегодня осталось {count} {current_cur}')
 
-
+            else:
+                print('Ошибка, неправильно введена валюта!')
 
 
 class CaloriesCalculator(Calculator):
@@ -78,7 +85,4 @@ class CaloriesCalculator(Calculator):
             stats = limit - today_stats
             print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {stats} кКал')
         elif today_stats >= limit:
-            print('Хватит есть!')    
-
-
-ref = CashCalculator.add_record(Record(1000, 'sss'))
+            print('Хватит есть!')
